8e8666420ed2b65ba4d327c55792945eb82c2097
Will Chen <willchen90@gmail.com>
2014-08-04 13:48:53 -0700

    Completed Koans

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -1,71 +1,74 @@
 describe("About Functions", function() {
 
   it("should declare functions", function() {
-    
+
     function add(a, b) {
       return a + b;
     }
-    
-    expect(add(1, 2)).toBe(FILL_ME_IN);
+
+    expect(add(1, 2)).toBe(3);
   });
 
   it("should know internal variables override outer variables", function () {
     var message = "Outer";
-    
+
     function getMessage() {
       return message;
     }
-    
+
     function overrideMessage() {
       var message = "Inner";
       return message;
     }
-    
-    expect(getMessage()).toBe(FILL_ME_IN);
-    expect(overrideMessage()).toBe(FILL_ME_IN);
-    expect(message).toBe(FILL_ME_IN);
+
+    expect(getMessage()).toBe("Outer");
+    expect(overrideMessage()).toBe("Inner");
+    expect(message).toBe("Outer");
   });
 
   it("should have lexical scoping", function () {
     var variable = "top-level";
     function parentfunction() {
-        var variable = "local";
+      var variable = "local";
       function childfunction() {
-          return variable;
+        return variable;
       }
       return childfunction();
     }
-    expect(parentfunction()).toBe(FILL_ME_IN);
+    expect(parentfunction()).toBe("local");
   });
 
   it("should use lexical scoping to synthesise functions", function () {
-    
-    function makeIncreaseByFunction(increaseByAmount) {
-      return function (numberToIncrease) {
-        return numberToIncrease + increaseByAmount;
+
+    function makeMysteryFunction(makerValue)
+    {
+      var newFunction = function doMysteriousThing(param)
+      {
+        return makerValue + param;
       };
+      return newFunction;
     }
-    
-    var increaseBy3 = makeIncreaseByFunction(3);
-    var increaseBy5 = makeIncreaseByFunction(5);
-    
-    expect(increaseBy3(10) + increaseBy5(10)).toBe(FILL_ME_IN);
+
+    var mysteryFunction3 = makeMysteryFunction(3);
+    var mysteryFunction5 = makeMysteryFunction(5);
+
+    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(23);
   });
 
   it("should allow extra function arguments", function () {
-    
+
     function returnFirstArg(firstArg) {
       return firstArg;
     }
-    
-    expect(returnFirstArg("first", "second", "third")).toBe(FILL_ME_IN);
-    
+
+    expect(returnFirstArg("first", "second", "third")).toBe("first");
+
     function returnSecondArg(firstArg, secondArg) {
       return secondArg;
     }
-    
-    expect(returnSecondArg("only give first arg")).toBe(FILL_ME_IN);
-    
+
+    expect(returnSecondArg("only give first arg")).toBe(undefined);
+
     function returnAllArgs() {
       var argsArray = [];
       for (var i = 0; i < arguments.length; i += 1) {
@@ -73,8 +76,8 @@ describe("About Functions", function() {
       }
       return argsArray.join(",");
     }
-    
-    expect(returnAllArgs("first", "second", "third")).toBe(FILL_ME_IN);
+
+    expect(returnAllArgs("first", "second", "third")).toBe("first,second,third");
   });
 
   it("should pass functions as values", function () {
@@ -82,27 +85,16 @@ describe("About Functions", function() {
     var appendRules = function (name) {
       return name + " rules!";
     };
-    
+
     var appendDoubleRules = function (name) {
       return name + " totally rules!";
     };
-    
-    var praiseSinger = { givePraise: appendRules };
-    expect(praiseSinger.givePraise("John")).toBe(FILL_ME_IN);
-    
-    praiseSinger.givePraise = appendDoubleRules;
-    expect(praiseSinger.givePraise("Mary")).toBe(FILL_ME_IN);
-      
-  });
 
-  it("should use function body as a string", function () {
-    var add = new Function("a", "b", "return a + b;");
-    expect(add(1, 2)).toBe(FILL_ME_IN);
-     
-    var multiply = function (a, b) {
-      //An internal comment
-      return a * b;
-    };
-    expect(multiply.toString()).toBe(FILL_ME_IN);
-  });    
+    var praiseSinger = { givePraise: appendRules };
+    expect(praiseSinger.givePraise("John")).toBe("John rules!");
+
+    praiseSinger.givePraise = appendDoubleRules;
+    expect(praiseSinger.givePraise("Mary")).toBe("Mary totally rules!");
+
+  });
 });
--- koans/AboutInheritance.js
+++ koans/AboutInheritance.js
@@ -1,7 +1,7 @@
 function Muppet(age, hobby) {
   this.age = age;
-  this.hobby = hobby; 
-  
+  this.hobby = hobby;
+
   this.answerNanny = function(){
 	return "Everything's cool!";
   }
@@ -10,7 +10,7 @@ function Muppet(age, hobby) {
 function SwedishChef(age, hobby, mood) {
   Muppet.call(this, age, hobby);
   this.mood = mood;
-  
+
   this.cook = function() {
     return "Mmmm soup!";
   }
@@ -21,24 +21,24 @@ SwedishChef.prototype = new Muppet();
 describe("About inheritance", function() {
   beforeEach(function(){
     this.muppet = new Muppet(2, "coding");
-  	this.swedishChef = new SwedishChef(2, "cooking", "chillin");
+	this.swedishChef = new SwedishChef(2, "cooking", "chillin");
   });
-  
+
   it("should be able to call a method on the derived object", function() {
-    expect(this.swedishChef.cook()).toEqual(FILL_ME_IN);
+    expect(this.swedishChef.cook()).toEqual("Mmmm soup!");
   });
-  
+
   it("should be able to call a method on the base object", function() {
-    expect(this.swedishChef.answerNanny()).toEqual(FILL_ME_IN);
+    expect(this.swedishChef.answerNanny()).toEqual("Everything's cool!");
   });
-  
+
   it("should set constructor parameters on the base object", function() {
-    expect(this.swedishChef.age).toEqual(FILL_ME_IN);
-    expect(this.swedishChef.hobby).toEqual(FILL_ME_IN);
+    expect(this.swedishChef.age).toEqual(2);
+    expect(this.swedishChef.hobby).toEqual("cooking");
   });
-  
+
   it("should set constructor parameters on the derived object", function() {
-    expect(this.swedishChef.mood).toEqual(FILL_ME_IN);
+    expect(this.swedishChef.mood).toEqual("chillin");
   });
 });
 
@@ -52,7 +52,7 @@ Object.prototype.beget = function () {
 function Gonzo(age, hobby, trick) {
   Muppet.call(this, age, hobby);
   this.trick = trick;
-  
+
   this.doTrick = function() {
     return this.trick;
   }
@@ -60,30 +60,26 @@ function Gonzo(age, hobby, trick) {
 
 //no longer need to call the Muppet (base type) constructor
 Gonzo.prototype = Muppet.prototype.beget();
-//note: if you're wondering how this line affects the below tests, the answer is that it doesn't.
-//however, it does do something interesting -- it makes this work:
-//  var g = new Gonzo(...);
-//  g instanceOf Muppet //true
 
 describe("About Crockford's inheritance improvement", function() {
   beforeEach(function(){
-    this.gonzo = new Gonzo(3, "daredevil performer", "eat a tire");
+  this.gonzo = new Gonzo(3, "daredevil performer", "eat a tire");
   });
-  
+
   it("should be able to call a method on the derived object", function() {
-    expect(this.gonzo.doTrick()).toEqual(FILL_ME_IN);
+    expect(this.gonzo.doTrick()).toEqual("eat a tire");
   });
-  
+
   it("should be able to call a method on the base object", function() {
-    expect(this.gonzo.answerNanny()).toEqual(FILL_ME_IN);
+    expect(this.gonzo.answerNanny()).toEqual("Everything's cool!");
   });
-  
+
   it("should set constructor parameters on the base object", function() {
-    expect(this.gonzo.age).toEqual(FILL_ME_IN);
-    expect(this.gonzo.hobby).toEqual(FILL_ME_IN);
+    expect(this.gonzo.age).toEqual(3);
+    expect(this.gonzo.hobby).toEqual("daredevil performer");
   });
-  
+
   it("should set constructor parameters on the derived object", function() {
-    expect(this.gonzo.trick).toEqual(FILL_ME_IN);
+    expect(this.gonzo.trick).toEqual("eat a tire");
   });
 });
